2467e3a6c91c143a5eabb5e53bc287482a268274
Daniel Hwang <whadmia@gmail.com>
2026-08-02 19:28:52 -0500

    nix Remove numtide/devshell flake

--- flake.nix
+++ flake.nix
@@ -2,21 +2,14 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    devshell = {
-      url = "github:numtide/devshell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;}
     {
-      imports = [
-        inputs.devshell.flakeModule
-      ];
       systems = ["x86_64-linux"];
       perSystem = {pkgs, ...}: {
-        devshells.default = {
+        devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             (python3.withPackages (python-pkgs: [
               python-pkgs.keymap-drawer
